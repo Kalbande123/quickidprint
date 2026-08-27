93d9eca56f4c6fc0843b38d7ae9c3ea140a708d3
Kalbande123 <saibabaaniaaibaba@gmail.com>
2026-08-27 22:11:04 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+const CONFIG = {
+    // तुमची नवीन Google Apps Script Web App URL
+    SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzoqgOKJ6Ej-NaeaqFCFVM8hHHNvUfdVSKfizKzg6-d6q1YOHkF2p-zhIjw88TtQLYi7w/exec",
+    
+    // आता आपण सबस्क्रिप्शन (Unlimited) मॉडेल वापरत आहोत, त्यामुळे येथे "Unlimited" सेट केले आहे
+    GHOSHANA_TOKEN_COST: "Unlimited" 
+};
