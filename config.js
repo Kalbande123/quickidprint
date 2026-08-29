b5195fe2940503e2f977cdce2bce382e693992aa
Kalbande123 <saibabaaniaaibaba@gmail.com>
2026-08-29 20:04:31 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     // तुमची नवीन Google Apps Script Web App URL
-    SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzUs2TjsonHjUGdlKRfla426PSph9g9cKXxEeo-J743QOlDM8jHEwACqA8sTNXOgJ1pNA/exec",
+    SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwdkB01JqfUHQ0UIb3-KuOk6tZUsLdp1_bicbmHOiMbprb3osmrirWv5rY6s1koPpoKLg/exec",
     
     // आता आपण सबस्क्रिप्शन (Unlimited) मॉडेल वापरत आहोत, त्यामुळे येथे "Unlimited" सेट केले आहे
     GHOSHANA_TOKEN_COST: "Unlimited" 
