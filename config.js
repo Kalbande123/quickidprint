c6654aca88378f55ba57ec6e5106a5fe9d9506c0
Kalbande123 <saibabaaniaaibaba@gmail.com>
2026-09-02 22:50:06 +0530

    Change SCRIPT_URL in config.js
    Updated SCRIPT_URL to a new Google Apps Script Web App URL.

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     // तुमची नवीन Google Apps Script Web App URL
-    SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyoXooYUGUNjJdZ_YKtydUzcZMAP2JbheOWRue-gt_ciYDMI-Zr-EJTfc-s2q2hd8TNDA/exec",
+    SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyqR1LuHbcWtdJZMuI0FX7l7Has4STJRKH1yBoyXgNsiV9cxGwEpTRa61NfF3NbvV0wrw/exec",
     
     // आता आपण सबस्क्रिप्शन (Unlimited) मॉडेल वापरत आहोत, त्यामुळे येथे "Unlimited" सेट केले आहे
     GHOSHANA_TOKEN_COST: "Unlimited" 
